97bc29b3b82671f171fd96cbcc6e54972c36b86d
Ryan Cheung <ryanc@livefyre.com>
2013-10-24 12:20:43 -0700

    Add bdd mocha tests

--- test/main.js
+++ test/main.js
@@ -1,11 +1,29 @@
-var should = require('should');
-var Encoder = require('../lib/main');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var LFCollectionMetaEncoder = require('../lib/main');
 
-describe('Encoder', function() {
+describe('LFCollectionMetaEncoder', function() {
+    var collectionMeta = {
+        networkId: 'livefyre.com',
+        siteId: '12345',
+        articleId: 'blah',
+        type: 'livecomments'
+    };
+    var siteKey = 'WOWZERS';
+
     describe('with no arguments', function() {
-        //it('returns an empty array', function() {
-        //    var result = sleepsort();
-        //    result.should.eql([]);
-        //});
+        it('is an instance of LFCollectionMetaEncoder', function() {
+            var encoder = new LFCollectionMetaEncoder();
+            expect(encoder).to.be.instanceOf(LFCollectionMetaEncoder);
+        });
+    });
+
+    describe('can encode collectionMeta', function () {
+        it('calls #encode', function () {
+            var encoder = new LFCollectionMetaEncoder();
+            var spy = sinon.spy(encoder, '_serializedJWT');
+            encoder.encode(collectionMeta, siteKey);
+            expect(spy.called).to.be.true;
+        });
     });
 });
