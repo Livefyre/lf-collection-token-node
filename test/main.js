60ad31ffe80d172140edaa3baf4ca2a7c4d85d05
Ryan Cheung <ryanc@livefyre.com>
2013-10-24 10:55:20 -0700

    Add module files

---
+++ test/main.js
@@ -0,0 +1,11 @@
+var should = require('should');
+var Encoder = require('../lib/main');
+
+describe('Encoder', function() {
+    describe('with no arguments', function() {
+        //it('returns an empty array', function() {
+        //    var result = sleepsort();
+        //    result.should.eql([]);
+        //});
+    });
+});
