6c9c8daba1455da62fe42cbe551a569fcc8bd955
Ryan Cheung <ryanc@livefyre.com>
2013-10-24 12:38:40 -0700

    Test return value of collection meta jwt encoder

--- test/main.js
+++ test/main.js
@@ -19,11 +19,30 @@ describe('LFCollectionMetaEncoder', function() {
     });
 
     describe('can encode collectionMeta', function () {
-        it('calls #encode', function () {
+        it('has #encode method', function () {
             var encoder = new LFCollectionMetaEncoder();
             var spy = sinon.spy(encoder, '_serializedJWT');
+
             encoder.encode(collectionMeta, siteKey);
             expect(spy.called).to.be.true;
+        });
+    });
+
+    describe('can return serialized JWT', function () {
+        it('expects collectionMeta and siteKey arguments', function () {
+            var encoder = new LFCollectionMetaEncoder();
+            var spy = sinon.spy(encoder, '_serializedJWT');
+
+            encoder.encode(collectionMeta, siteKey);
+            expect(spy.withArgs(collectionMeta, siteKey).called).to.be.true;
+        });
+
+        it('returns serialized JWT', function () {
+            var encoder = new LFCollectionMetaEncoder();
+            var spy = sinon.spy(encoder, '_serializedJWT');
+
+            var serializedJWT = encoder.encode(collectionMeta, siteKey);
+            expect(serializedJWT).to.equal('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuZXR3b3JrSWQiOiJsaXZlZnlyZS5jb20iLCJzaXRlSWQiOiIxMjM0NSIsImFydGljbGVJZCI6ImJsYWgiLCJ0eXBlIjoibGl2ZWNvbW1lbnRzIiwiY2hlY2tzdW0iOiJjNmU1YzhjNTZkNDk2MmRlMTU3NDJlZjdlODU1YTdhMyJ9.V0haRVZWVTFaV2RVUkVGMWRIa3hTamhYYlVseFNFdHNRamRITWxGT0wwbFNjV1phVTNvMFZ6WnNUVDA9');
         });
     });
 });
